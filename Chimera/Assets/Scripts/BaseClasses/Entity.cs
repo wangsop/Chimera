768ed9dc2bd0b35f2950c5c9252b0173a2b43453
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2025-05-16 11:31:16 -0400

    Update Entity

--- Chimera/Assets/Scripts/BaseClasses/Entity.cs
+++ Chimera/Assets/Scripts/BaseClasses/Entity.cs
@@ -1,17 +1,6 @@
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+public interface Entity
 {
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
