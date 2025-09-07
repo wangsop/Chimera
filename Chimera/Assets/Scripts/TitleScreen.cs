e55c83db9c2d86429c645065b61bc0335f1f035b
gianlucaluca <gianlucagl17@gmail.com>
2025-09-07 11:11:08 -0400

    Added ESC to quit and debug message

--- Chimera/Assets/Scripts/TitleScreen.cs
+++ Chimera/Assets/Scripts/TitleScreen.cs
@@ -13,13 +13,16 @@ public class TitleScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
     }
     public void EnterLab() {
         SceneManager.LoadScene("Lab");
     }
     public void QuitGame() {
+        Debug.Log("Quitting game! This would've quit in an actual build.");
         Application.Quit(); // only works when built
-
     }
 }
