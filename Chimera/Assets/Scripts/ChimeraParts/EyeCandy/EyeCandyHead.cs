9c3f1bbb095badacecc7f37e57c67fa4f6ffdf89
ethan-rodgers <ethanbrodgers@gmail.com>
2025-09-20 13:25:00 -0400

    Eye Candy ability (untested)
    Causes all creatures of opposite type (monster/chimera) to aggro-inate this Eye Candy, then reverses the effect a few seconds later. Does not reverse the effect for creatures that are now close enough to attack Eye Candy naturally.

--- Chimera/Assets/Scripts/ChimeraParts/EyeCandy/EyeCandyHead.cs
+++ Chimera/Assets/Scripts/ChimeraParts/EyeCandy/EyeCandyHead.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
 
 [DefaultExecutionOrder(-100)]
 public class EyeCandyHead : Head
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public override void UseAbility(){
+    public static readonly UnityEvent<Creature, double> onEyeTrigger = new();
+    public static readonly UnityEvent<Creature, double> onEyeTriggerEnd = new();
+    [SerializeField] private static readonly double distractRadius = 5;
+    [SerializeField] private static readonly float abilityDuration = 5;
+    private Creature thisCreature;
+
+    // broadcasts event to attract attention, then starts coroutine delay then end the effect
+    public override void UseAbility()
+    {
         Debug.Log("Used Eye Candy Ability");
+        onEyeTrigger.Invoke(thisCreature, distractRadius);
+        StartCoroutine(DelayStopAbility());
     }
-    protected override void Initialize(){
+
+    // coroutine that, after ability duration ends, broadcasts the event onEyeTriggerEnd
+    private IEnumerator DelayStopAbility()
+    {
+        yield return new WaitForSeconds(abilityDuration);
+        onEyeTriggerEnd.Invoke(thisCreature, distractRadius);
+    }
+
+    protected override void Initialize()
+    {
         index = 3;
+        thisCreature = GetComponentInParent<Creature>();
         base.Initialize();
     }
 }
--- Chimera/Assets/Scripts/Creature.cs
+++ Chimera/Assets/Scripts/Creature.cs
@@ -5,22 +5,22 @@ using System;
 public abstract class Creature : MonoBehaviour, Entity
 {
     protected bool hostile;
-     protected int health = 10;
-     protected int maxHealth = 10;
-     protected int attack = 1;
+    protected int health = 10;
+    protected int maxHealth = 10;
+    protected int attack = 1;
     //[SerializeField] protected Collider2D collisions;
-     protected Rigidbody2D rgb;
+    protected Rigidbody2D rgb;
     [SerializeField] protected Collider2D trig;
-     protected Creature aggro;
-     protected int clock = 0;
-     protected List<Creature> inTrigger;
+    protected Creature aggro;
+    protected int clock = 0;
+    protected List<Creature> inTrigger;
     [SerializeField] protected int attackRange = 15;
     [SerializeField] protected int speed = 300;
-     int attackCount = 0;
-     protected Head head;
-     protected Body body;
-     protected Tail tail;
-     public event Action<float> OnHealthChanged = delegate { };
+    int attackCount = 0;
+    protected Head head;
+    protected Body body;
+    protected Tail tail;
+    public event Action<float> OnHealthChanged = delegate { };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected void Start()
     {
@@ -36,17 +36,23 @@ public abstract class Creature : MonoBehaviour, Entity
         head.GetComponent<Animator>().SetBool("IsChimera", !hostile);
         body.GetComponent<Animator>().SetBool("IsChimera", !hostile);
         tail.GetComponent<Animator>().SetBool("IsChimera", !hostile);
+
+        // event responses
+        EyeCandyHead.onEyeTrigger.AddListener(OnEyeTriggerResponse);
+        EyeCandyHead.onEyeTriggerEnd.AddListener(OnEyeTriggerEndResponse);
     }
 
     // Update is called once per frame
     protected void Update()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, 30f);
-        foreach (Collider col in colliders) {
+        foreach (Collider col in colliders)
+        {
             Debug.Log("Overlap detected with: " + col.gameObject.name);
         }
         clock++;
-        if (aggro != null) {
+        if (aggro != null)
+        {
             //head towards object of aggro
             Vector2 aggPos = new Vector2(aggro.gameObject.GetComponent<Transform>().position.x, aggro.gameObject.GetComponent<Transform>().position.y);
             Vector2 pos = (Vector2)transform.position;
@@ -54,8 +60,11 @@ public abstract class Creature : MonoBehaviour, Entity
             {
                 Vector2 newPos = Vector2.MoveTowards(transform.position, aggPos, speed * Time.deltaTime);
                 rgb.MovePosition(newPos);
-            } else {
-                if (clock > 1000) {
+            }
+            else
+            {
+                if (clock > 1000)
+                {
                     clock = 0;
                     attackCount++;
                     Debug.Log("Attacking");
@@ -65,23 +74,27 @@ public abstract class Creature : MonoBehaviour, Entity
         }
     }
 
-    public bool takeDamage(int dmg) {
+    public bool takeDamage(int dmg)
+    {
         Debug.Log(hostile ? "Enemy took damage" : "Ally took damage");
         dmg = body.takeDamage(dmg);
         health -= dmg;
-        float healthPercent = (float) health / (float) maxHealth;
+        float healthPercent = (float)health / (float)maxHealth;
         OnHealthChanged(healthPercent);
-        if (health <= 0) {
+        if (health <= 0)
+        {
             Die();
             return true;
         }
         return false;
     }
 
-    public void Attack(Creature target) {
+    public void Attack(Creature target)
+    {
         Debug.Log(hostile ? "Enemy attacked" : "Ally attacked");
         bool died = tail.Attack(target);
-        if (died) {
+        if (died)
+        {
             aggro = null;
             reAggro();
             if (this.hostile == false)
@@ -89,46 +102,103 @@ public abstract class Creature : MonoBehaviour, Entity
                 Globals.energy += 5;
             }
         }
-    } 
+    }
 
-    public void Die() {
+    public void Die()
+    {
         head.animator.SetBool("IsAlive", false);
         body.animator.SetBool("IsAlive", false);
         tail.animator.SetBool("IsAlive", false);
         Destroy(this.gameObject);
     }
 
-    protected void OnTriggerEnter2D(Collider2D other) {
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
         //Debug.Log("New trigger enter");
-        if (aggro == null && (other.gameObject.GetComponent<Creature>() != null)) {
-            if (other.gameObject.GetComponent<Creature>().hostile != hostile) {
+        if (aggro == null && (other.gameObject.GetComponent<Creature>() != null))
+        {
+            if (other.gameObject.GetComponent<Creature>().hostile != hostile)
+            {
                 aggro = other.gameObject.GetComponent<Creature>(); //only aggro if it's an enemy Creature
                 Debug.Log("Aggroed");
             }
-        } else if (other.gameObject.GetComponent<Creature>() != null && other.gameObject.GetComponent<Creature>().hostile != hostile) {
+        }
+        else if (other.gameObject.GetComponent<Creature>() != null && other.gameObject.GetComponent<Creature>().hostile != hostile)
+        {
             inTrigger.Add(other.gameObject.GetComponent<Creature>());
         }
     }
-    protected void OnCollisionEnter2D(Collision2D other) {
+    protected void OnCollisionEnter2D(Collision2D other)
+    {
         Debug.Log("Collision");
     }
     //so right now, the first enemy to enter trigger is aggro'd onto until it dies or leaves the trigger (when eyeball moves)
-    protected void OnTriggerExit2D(Collider2D other) {
+    protected void OnTriggerExit2D(Collider2D other)
+    {
         //Debug.Log("Trigger Exit");
-        if (other != null && aggro != null) {
-            if (other.gameObject == aggro.gameObject) {
+        if (other != null && aggro != null)
+        {
+            if (other.gameObject == aggro.gameObject)
+            {
                 aggro = null; //if currently aggro'd object leaves trigger colllider, stops aggroing it
                 reAggro();
-            } else if (other.gameObject.GetComponent<Creature>() != null) {
+            }
+            else if (other.gameObject.GetComponent<Creature>() != null)
+            {
                 inTrigger.Remove(other.gameObject.GetComponent<Creature>());
             }
         }
     }
-    protected void reAggro() {
+    protected void reAggro()
+    {
         Debug.Log("New aggro");
-        if (inTrigger.Count > 0){
+        if (inTrigger.Count > 0)
+        {
             aggro = inTrigger[0]; //take off a Creature in the collider, make that new aggro target
             inTrigger.RemoveAt(0);
         }
+    }
+
+    // Eye Candy's distract ability: adds the Eye Candy head's chimera to the front of this creature's inTrigger
+    protected void OnEyeTriggerResponse(Creature eyeCandy, double distractRadius)
+    {
+        Debug.Log($"{this} heard Eye Candy's ability");
+
+        // guard clause: on opposing teams
+        if (this.hostile == eyeCandy.IsHostile()) return;
+        // guard clause: close enough
+        if (DistanceTo(eyeCandy) > distractRadius) return;
+
+        // distract
+        Debug.Log($"{this} was distracted by Eye Candy");
+        inTrigger.Insert(0, eyeCandy);
+        reAggro();
+    }
+
+    // Eye Candy's distract ability: removes the Eye Candy head's chimera from the front of this creature's inTrigger if present
+    protected void OnEyeTriggerEndResponse(Creature eyeCandy, double distractRadius)
+    {
+        Debug.Log($"{this} heard Eye Candy's ability end");
+
+        // if moving towards Eye Candy but not yet close enough to attack, stop targeting it
+        if (inTrigger.Count > 0 && inTrigger[0] == eyeCandy && DistanceTo(eyeCandy) > distractRadius)
+        {
+            Debug.Log($"{this} lost interest in Eye Candy");
+            inTrigger = inTrigger.GetRange(1, inTrigger.Count - 1);
+            reAggro();
+        }
+    }
+
+
+    // getter for hostile
+    public bool IsHostile()
+    {
+        return hostile;
+    }
+
+    // helper function: distance from this to a game obj
+    private double DistanceTo(MonoBehaviour mb)
+    {
+        return (mb.GetComponent<Transform>().position - transform.position).magnitude;
     }
 }
