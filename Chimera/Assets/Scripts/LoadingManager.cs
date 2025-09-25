34de929d28249eece3725e1a5b59852ff94ff74a
gianlucaluca <gianlucagl17@gmail.com>
2025-09-25 15:56:43 -0400

    Replaced dungeon loading in globals, modified scene transitions to use new system

--- Chimera/Assets/Scripts/LoadingManager.cs
+++ Chimera/Assets/Scripts/LoadingManager.cs
@@ -4,14 +4,52 @@ using UnityEngine.SceneManagement;
 
 public class LoadingManager : MonoBehaviour
 {
-    void Start()
+    public static String NextSceneToLoad;
+
+    // This should be used most of the time. type the needed scene in the inspector
+    public static void LoadScene(String scene) 
     {
-        DontDestroyOnLoad(gameObject);
-    }
-    public void LoadScene(String scene) 
-    {
-        LoadingScreen.NextSceneToLoad = scene;
+        NextSceneToLoad = scene;
         SceneManager.LoadScene("Loading Screen");
     }
 
+    // Use this for the "Level x" buttons in the Level Select scene.
+    // Type the dungeon # in the inspector 
+    public static void DungeonQueue(int SelectedLevel)
+    {
+        // replaces the globals level check
+        if (SelectedLevel > Globals.highestClearedLevel)
+        {
+            Debug.Log("You have not unlocked this level yet!");
+        } 
+        else
+        {
+            if (SelectedLevel > 0)
+            {
+                NextSceneToLoad = "Dungeon" + SelectedLevel;
+            }
+            else
+            {
+                NextSceneToLoad = "Dungeon";
+            }
+            Debug.Log("Selected " + NextSceneToLoad);
+            SceneManager.LoadScene("Chimera Select");
+        }
+    }
+
+    // Mostly identical to LoadScene, however it uses the previously saved NextSceneToLoad
+    // to keep the dungeon # for the next loading screen call. 
+    // It also checks to make sure the party isn't empty before entering the dungeon. 
+    public static void LoadDungeon()
+    {
+        if (Globals.party_indexes.Count > 0)
+        {
+            SceneManager.LoadScene("Loading Screen");
+        } 
+        else
+        {
+            Debug.Log("You must pick at least one Chimera before going into the dungeon!");
+        }
+        
+    }
 }
--- Chimera/Assets/Scripts/LoadingScreen.cs
+++ Chimera/Assets/Scripts/LoadingScreen.cs
@@ -9,26 +9,26 @@ public class LoadingScreen : MonoBehaviour
     [SerializeField] 
     private Image LoadingBar;
 
-    public static String NextSceneToLoad;
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(LoadSceneAsync(NextSceneToLoad));
+        StartCoroutine(LoadSceneAsync(LoadingManager.NextSceneToLoad));
     }
 
     public IEnumerator LoadSceneAsync(string scene)
     {
         
         AsyncOperation LoadSceneAsync = SceneManager.LoadSceneAsync(scene);
+        LoadSceneAsync.allowSceneActivation = true;
 
         
+
         while (!LoadSceneAsync.isDone)
         {
-            LoadingBar.fillAmount = Mathf.Clamp01(LoadSceneAsync.progress / 9f);
+            LoadingBar.fillAmount = Mathf.Clamp01(LoadSceneAsync.progress / .9f);
             yield return null;
         }
+        
         LoadingBar.fillAmount = 0;
     }
 }
