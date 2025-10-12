f2c98342a03cde9c675dd666a02524be6cd7e5e9
Rachael Setia <rachaelsetia@gmail.com>
2025-10-12 16:17:17 -0400

    Added Lichen Slug Whirlpool Ability
    - useAbility spawns in a whirlpool that moves in the direction of the slug and destroys itself after a specified amount of time (set to 2 seconds for now)
    - added Lichen Whirlpool prefab with movement/destruction behavior dictated by LichenWhirlpoolBehavior
    - added WhirlpoolSpawnPoint as child of LichenSlugHeadPrefab

--- Chimera/Assets/Scripts/ChimeraParts/Slug/LichenSlugHead.cs
+++ Chimera/Assets/Scripts/ChimeraParts/Slug/LichenSlugHead.cs
@@ -3,12 +3,35 @@ using UnityEngine;
 [DefaultExecutionOrder(-100)]
 public class LichenSlugHead : Head
 {
+    [SerializeField] private GameObject whirlpool;
+    [SerializeField] private Transform whirlpoolSpawnPoint;
+
+    private GameObject whirlpoolInstance;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public override void UseAbility(){
+    public override void UseAbility()
+    {
         Debug.Log("Used Lichen Slug Ability");
+
+        // spawn whirlpool
+        whirlpoolInstance = Instantiate(whirlpool, whirlpoolSpawnPoint.position, Quaternion.identity);
+        Debug.Log(whirlpoolSpawnPoint.position);
     }
-    protected override void Initialize(){
+    protected override void Initialize()
+    {
         index = 0;
         base.Initialize();
     }
+
+    // honestly not sure when the UseAbility() function is called, so I'm testing with an Update() function
+    /*
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            // spawn whirlpool
+            whirlpoolInstance = Instantiate(whirlpool, whirlpoolSpawnPoint.position, Quaternion.identity);
+        }
+    }
+    */
 }
