24628419083da553cf263becfdd33734e8ae435d
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2025-10-12 17:18:46 -0400

    Adding Splash Art + Descriptions
    - gave descriptions to Head class with default values
    - added splash art for every prefab

--- Chimera/Assets/Scripts/Head.cs
+++ Chimera/Assets/Scripts/Head.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public abstract class Head : BodyPart
 {
+    protected string ability_description = "Ability description has not been implemented yet";
+    protected string scientist_description = "Scientist description not been implemented yet";
     public abstract void UseAbility();
     protected override void Initialize(){
         //image = GameObject.Find("Main Camera").GetComponent<Globals>().Heads[index];
