b38e2d84b4a97577245679e1a7c68996ae8255a3
wangsop <sophia.riverfront@gmail.com>
2025-10-28 20:38:08 -0400

    Procedural generation level integrated; eyeball adjust
    game now sends you to procedurally generated levels instead of normal level 1; tilemap is old and needs to be replaced; eyeball has been added to the scene + camera, so in theory once we fix the sizing on literally everything it'll work?

--- Chimera/Assets/Scripts/Procedural_Generation/Dungeon_Generation/RunDungeon.cs
+++ Chimera/Assets/Scripts/Procedural_Generation/Dungeon_Generation/RunDungeon.cs
@@ -4,7 +4,7 @@ public class RunDungeon : MonoBehaviour
 {
     public CorridorFirstDungeonGeneration cfdg;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         cfdg.RunProceduralGeneration();
     }
