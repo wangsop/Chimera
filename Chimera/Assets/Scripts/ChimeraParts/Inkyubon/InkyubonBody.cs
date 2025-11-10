a9d8706dff40f780ef7f074fdb3581a287ae2d20
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2025-11-10 05:16:06 -0500

    The MPreg Update
    - Yep it's officially in the game now
    - Hooray
    - It says I'm committing 69 files so uh take that as you will

---
+++ Chimera/Assets/Scripts/ChimeraParts/Inkyubon/InkyubonBody.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(-100)]
+public class InkyubonBody : Body
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Initialize(){
+        health = 7;
+        base.Initialize();
+    }
+    protected override void Update(){
+        
+    }
+}
