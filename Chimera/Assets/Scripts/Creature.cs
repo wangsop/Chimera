783096b843e33043b70ae77d10aac991b1869793
gianlucaluca <gianlucagl17@gmail.com>
2025-11-13 00:17:39 -0500

    removed random white space (i forgot)

--- Chimera/Assets/Scripts/Creature.cs
+++ Chimera/Assets/Scripts/Creature.cs
@@ -34,7 +34,6 @@ public abstract class Creature : Damageable_Testing, Entity
     protected bool canAttack = true;
     private Coroutine _stunRoutine;
     private Coroutine _slowRoutine;
-    
 
     public void OnAfflicted(bool stunned, bool slowed, float speedReduction, float duration)
     {
